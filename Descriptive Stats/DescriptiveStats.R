9818a4f74f021b3a2bb7e23ca5d3f460a2a0c20d
lukewilson2716 <144976758+lukewilson2716@users.noreply.github.com>
2026-03-06 12:46:04 -0500

    idk

---
+++ Descriptive Stats/DescriptiveStats.R
@@ -0,0 +1,4 @@
+read.csv("2025Data - 1-2 (1).csv")
+myData <- read.csv("2025Data - 1-2 (1).csv", skip = 1, header = TRUE)
+myData$Demographics_age <- as.numeric(myData$Demographics_age)
+summary(myData$Demographics_age)
--- data/DescriptiveStats.R
+++ data/DescriptiveStats.R
@@ -1,7 +1,3 @@
-read.csv("2025Data - 1-2 (1).csv")
-myData <- read.csv("2025Data - 1-2 (1).csv", skip = 1, header = TRUE)
-myData$Demographics_age <- as.numeric(myData$Demographics_age)
 
-summary(myData$Demographics_age)
-summary(myData)
+
 
