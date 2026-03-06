ca8d116ae907153d93ee8e91745fd01fe9b8f21f
lukewilson2716 <144976758+lukewilson2716@users.noreply.github.com>
2026-03-06 13:48:27 -0500

    Actual Content now

---
+++ 2025Descriptives/DescriptiveStats.R
@@ -0,0 +1,9 @@
+read.csv("2025Data - 1-2 (1).csv")
+myData <- read.csv("2025Data - 1-2 (1).csv", header = TRUE)
+print(summary(myData$Psychological.Wellness))
+print(sd(myData$Psychological.Wellness, na.rm = TRUE))
+# Psychological Wellness Mean = 4.631, St. Dev = 0.7435167
+
+
+#Valuable Descriptive here blah blah
+
--- Descriptive Stats/DescriptiveStats.R
+++
@@ -1,7 +0,0 @@
-read.csv("2025Data - 1-2 (1).csv")
-myData <- read.csv("2025Data - 1-2 (1).csv", skip = 1, header = TRUE)
-myData$Demographics_age <- as.numeric(myData$Demographics_age)
-summary(myData$Demographics_age)
-
-print(summary(myData))
-#Valuable Descriptive here blah blah
