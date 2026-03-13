eb328d37c9a9f6a5e2852df792a77c89c3e25433
lukewilson2716 <144976758+lukewilson2716@users.noreply.github.com>
2026-03-13 10:38:52 -0400

    2025 analysis

--- Analysis/2025Descriptives/DescriptiveStats.R
+++ Analysis/2025Descriptives/DescriptiveStats.R
@@ -1,6 +1,6 @@
-read.csv("2025Data - 1-2 (1).csv")
+read.csv("Data/2025Data - 1-2 (1).csv")
 # Read the file
-myData <- read.csv("2025Data - 1-2 (1).csv", header = TRUE)
+myData <- read.csv("Data/2025Data - 1-2 (1).csv", header = TRUE)
 # Assign File to something, header true needed to count first row as header not data
 print(summary(myData$Psychological.Wellness))
 # This Gives Mean + Quartile info
@@ -21,3 +21,14 @@ model2 <- lm(Psychological.Wellness ~ myData$Demographics_rlgsty, data = myData)
 #linear regression for psych wellness and religiosity
 summary(model2)
 #P .212 > 0.05, no stat sig relationship between Psych Wellness and Religiosity
+
+model2 <- lm(Spiritual.Wellness ~ myData$Demographics_rlgsty, data = myData)
+summary(model2)
+ggplot(myData, aes(x = Demographics_rlgsty, y = Spiritual.Wellness)) +
+  geom_jitter(alpha = 0.5, color = "darkblue", width = 0.2, height = 0.2) +
+  geom_smooth(method = "lm", color = "red", se = TRUE) + # Adds a trend line
+  labs(title = "Religiosity vs. Spiritual Well-being",
+       x = "Religiosity Score (1-7)",
+       y = "Total Well-being Score (1-7)") +
+  theme_minimal()
+#Oh our datas goated
