4357c4c5651b926ce847a750da52132e1c6491be
Corinne Cutts <cococutts@cocos-MacBook-Air-3.local>
2026-03-20 10:03:25 -0400

    Added QQplot still need social well-being and down

--- Analysis/homogeneity_tests.R
+++ Analysis/homogeneity_tests.R
@@ -4,47 +4,45 @@ myData <- read.csv("Data/Complete_Data - 1.csv", header = TRUE)
 myData$Demographics_relig <- as.factor(myData$Demographics_relig)
 
 library(car)
+library(ggplot2)
 
 #Spiritual 
 model_spirit <- aov(Total_Spirit_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_spirit))
-qqnorm(residuals(model_spirit))
-qqline(residuals(model_spirit), col = "red")
-leveneTest(Total_Spirit_Wellbeing ~ Demographics_relig, data = myData)
+spirit_mod <- ggplot(data = data.frame(resid = residuals(model_spirit)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "red") +
+  ggtitle("Q-Q Plot: Spiritual Wellbeing Residuals")
+print(spirit_mod)
 
 #Psychological 
 model_psych <- aov(Total_Psych_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_psych))
-qqnorm(residuals(model_psych))
-qqline(residuals(model_psych), col = "red")
-leveneTest(Total_Psych_Wellbeing ~ Demographics_relig, data = myData)
+psych_mod<- ggplot(data = data.frame(resid = residuals(model_psych)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "blue") +
+  ggtitle("Q-Q Plot: Psychological Wellbeing Residuals")
+print(psych_mod)
+
 
 #Social 
 model_social <- aov(Total_Social_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_social))
-qqnorm(residuals(model_social))
-qqline(residuals(model_social), col = "red")
-leveneTest(Total_Social_Wellbeing ~ Demographics_relig, data = myData)
+
 
 #Emotional 
 model_emot <- aov(Total_Emot_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_emot))
-qqnorm(residuals(model_emot))
-qqline(residuals(model_emot), col = "red")
-leveneTest(Total_Emot_Wellbeing ~ Demographics_relig, data = myData)
+
 
 #Intellectual
 model_int <- aov(Total_Int_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_int))
-qqnorm(residuals(model_int))
-qqline(residuals(model_int), col = "red")
-leveneTest(Total_Int_Wellbeing ~ Demographics_relig, data = myData)
+
 
 #Physical 
 model_phys <- aov(Total_Physical_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_phys))
-qqnorm(residuals(model_phys))
-qqline(residuals(model_phys), col = "red")
-leveneTest(Total_Physical_Wellbeing ~ Demographics_relig, data = myData)
+
 
 
