2355122ac07cae5275341651f3e149655848b1bc
Corinne Cutts <cococutts@cocos-MacBook-Air-3.local>
2026-03-20 10:18:38 -0400

    updated

--- Analysis/homogeneity_tests.R
+++ Analysis/homogeneity_tests.R
@@ -28,21 +28,39 @@ print(psych_mod)
 #Social 
 model_social <- aov(Total_Social_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_social))
+social_mod<- ggplot(data = data.frame(resid = residuals(model_social)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "darkorchid") +
+  ggtitle("Q-Q Plot: Social Wellbeing Residuals")
+print(social_mod)
 
 
 #Emotional 
 model_emot <- aov(Total_Emot_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_emot))
-
+emot_mod<- ggplot(data = data.frame(resid = residuals(model_emot)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "darkolivegreen4") +
+  ggtitle("Q-Q Plot: Emotional Wellbeing Residuals")
+print(emot_mod)
 
 #Intellectual
 model_int <- aov(Total_Int_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_int))
-
+int_mod<- ggplot(data = data.frame(resid = residuals(model_int)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "orange") +
+  ggtitle("Q-Q Plot: Intellectual Wellbeing Residuals")
+print(int_mod)
 
 #Physical 
 model_phys <- aov(Total_Physical_Wellbeing ~ Demographics_relig, data = myData)
 shapiro.test(residuals(model_phys))
+physical_mod<- ggplot(data = data.frame(resid = residuals(model_phys)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "deeppink3") +
+  ggtitle("Q-Q Plot: Physical Wellbeing Residuals")
+print(physical_mod)
 
 
 
