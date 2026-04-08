3805aa59c72a290c3f8c23ce689d95335dd55be1
lukewilson2716 <144976758+lukewilson2716@users.noreply.github.com>
2026-04-08 17:11:19 -0400

    updated

--- Graphs/Updated Clustering.R
+++ Graphs/Updated Clustering.R
@@ -1,9 +1,11 @@
-
 read.csv("Data/new_complete_data - Sheet1.csv")
 # Read the file, Data/ to access it in the folder
 myData <- read.csv("Data/new_complete_data - Sheet1.csv", header = TRUE)
 # Assign File to something, header true needed to count first row as header not data
 library(dplyr)
+library(ggplots2)
+library(patchwork)
+#needed for this script
 
 myData_cleaned <- myData %>%
   mutate(Religion_Cluster = case_when(
@@ -38,16 +40,31 @@ summary(model2)
 
 covModel1 <- aov(Total_Int_Wellbeing ~ Religion_Cluster * Demographics_rlgsty, data = myData_cleaned)
 summary(covModel1)
+#nothing
 
 covModel2 <- aov(Total_Spirit_Wellbeing ~ Religion_Cluster * Demographics_rlgsty, data = myData_cleaned)
 summary(covModel2)
 #very cool results, both cluster and religiosity have sig results here
 
+covModel3 <- aov(Total_Psych_Wellbeing ~ Religion_Cluster * Demographics_rlgsty, data = myData_cleaned)
+summary(covModel3)
+#Clustered Anova for psych wellbeing, Religion had effect, cluster didnt
 
+covModel4 <- aov(Total_Emot_Wellbeing ~ Religion_Cluster * Demographics_rlgsty, data = myData_cleaned)
+summary(covModel4)
+#nothing
+
+covModel5 <- aov(Total_Social_Wellbeing ~ Religion_Cluster * Demographics_rlgsty, data = myData_cleaned)
+summary(covModel5)
+#Religion had effect
+
+covModel6 <- aov(Total_Physical_Wellbeing ~ Religion_Cluster * Demographics_rlgsty, data = myData_cleaned)
+summary(covModel6)
+#nothing
 
 title1 <- str_wrap("Overall Wellbeing", width = 30)
-title2 <- str_wrap("Emotional Wellbeing", width = 30)
-title3 <- str_wrap("Intellectual Wellbeing", width = 30)
+title2 <- str_wrap("Social Wellbeing", width = 30)
+title3 <- str_wrap("Spiritual Wellbeing", width = 30)
 
  a1 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Psych_Wellbeing, color = Religion_Cluster)) +
   geom_point(alpha = 0.4) + 
@@ -59,7 +76,7 @@ title3 <- str_wrap("Intellectual Wellbeing", width = 30)
   theme_minimal()
 #Psych wellbeing graph
 
-a2 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Emot_Wellbeing, color = Religion_Cluster)) +
+a2 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Social_Wellbeing, color = Religion_Cluster)) +
   geom_point(alpha = 0.4) + 
   geom_smooth(method = "lm", formula = y ~ x) + # Adds a linear regression line for each group
   labs(title = title2,
@@ -69,7 +86,7 @@ a2 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Emot_Wellbei
   theme_minimal()
 #emotional wellbeing graph
 
-a3 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Int_Wellbeing, color = Religion_Cluster)) +
+a3 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Spirit_Wellbeing, color = Religion_Cluster)) +
   geom_point(alpha = 0.4) + 
   geom_smooth(method = "lm", formula = y ~ x) + # Adds a linear regression line for each group
   labs(title = title3,
@@ -78,6 +95,16 @@ a3 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Int_Wellbein
        y = "Wellbeing Score") +
   theme_minimal()
 #int wellbeing graph
+a4 <- ggplot(myData_cleaned, aes(x = Demographics_rlgsty, y = Total_Spirit_Wellbeing, color = Religion_Cluster)) +
+  geom_point(alpha = 0.4) + 
+  geom_smooth(method = "lm", formula = y ~ x) + # Adds a linear regression line for each group
+  labs(title = title3,
+       subtitle = "By Religious Cluster",
+       x = "Religiosity Score",
+       y = "Wellbeing Score") +
+  theme_minimal()
+#int wellbeing graph
+a4
 
 a1 + a2 + a3 + plot_layout(guides = "collect") + plot_annotation(title = "Clustered Anaylsis of Religion, Religiosity, and their Relationship with Well-Being")
 #visualization for the poster maybe
